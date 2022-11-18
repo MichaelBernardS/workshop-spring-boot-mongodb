5c0866160a44a1483133d7ede2e35dedbf2541ed
Michael Bernard <mich4el_bernard@hotmail.com>
2022-11-18 17:46:43 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/uniondata/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniondata.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
